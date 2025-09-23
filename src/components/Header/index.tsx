5460193900e2505348d8a66f3b889d0622101fe9
pluto <valcano103@gmail.com>
2025-09-23 21:38:43 +0800

    feat: header

---
+++ src/components/Header/index.tsx
@@ -0,0 +1,18 @@
+import React from 'react'
+
+export default function Header() {
+  return (
+    <div className="flex justify-between items-center px-8 py-2">
+      <div className="text-2xl font-bold">
+        Vearah
+      </div>
+      <div className='flex gap-6'>
+        <button>Our Projects</button>
+        <button className="gradient-text">
+          Our Team
+        </button>
+        <button className="gradient-button">Book a free discovery call</button>
+      </div>
+    </div>
+  )
+}
